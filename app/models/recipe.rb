786dca92a97f808d5d657e5ad75961fbbe1a3b04
EC2 Default User <ec2-user@ip-172-31-33-91.ap-northeast-1.compute.internal>
2022-10-24 11:12:45 +0000

    image changed

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,11 +1,10 @@
 class Recipe < ApplicationRecord
+  paginates_per 8
   has_one_attached :image
   belongs_to :admin, optional: true
   belongs_to :user, optional: true
   belongs_to :genre
   has_many :ingredients
-
-  paginates_per 8
 
   def self.search_by(search_term)
     if search_term.present?
